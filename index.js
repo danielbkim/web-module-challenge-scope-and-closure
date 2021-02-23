7396f9da9d2f5f77f55d048956d5b43839e4f407
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-02-22 19:03:40 -0500

    adds all the passing tests to work on scoreboard()

--- index.js
+++ index.js
@@ -89,17 +89,27 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
-}
+function finalScore(func, inning){
+  let score = {
+    "Home": func() * inning,
+    "Away": func() * inning
+  }
+  return score;
+};
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
+function getInningScore(func) {
   /*Your Code Here */
+  let score = {
+    "Home": func(),
+    "Away": func()
+  }
+
+  return score;
 }
 
 
